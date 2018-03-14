cfa28c110aa852ea6b79782b731bc744f6997c11
Haldir <haldir@haldir.com>
2018-03-14 13:44:11 +0100

    ASDASDASDAS

--- openload.com/openload.com.user.js
+++ openload.com/openload.com.user.js
@@ -4,17 +4,24 @@
 // @include     https://oload.stream/*
 // @include     https://openload.co/*
 // @require     https://code.jquery.com/jquery-3.2.1.slim.min.js
-// @version  2
-// @grant    unsafeWindow
+// @version     3
+// @grant       unsafeWindow
 // ==/UserScript==
 
-// Set this to make the page think it got opened after doing the "switch to ad-page and open the vid in new tab" thing
+// Set this to make the page think it got opened after doing the 
+// "switch to ad-page and open the vid in new tab" thing
 unsafeWindow.ffopened = true;
 
 $(function() {
+  var video = $("<video>"); 
+  video.on("loadedmetadata", function(event) {
+    var duration = Math.round(video.get(0).duration);
+    console.log("duration found: ", duration);
+    unsafeWindow.top.postMessage(duration, "*");
+  });
   $("#videooverlay").click();
   var src = $("#olvideo_html5_api").attr("src");
-  var $video = $("<video>").attr({"src": src, controls: true});
+  var $video = video.attr({"src": src, controls: true});
   $video.css({width: "100vw", height: "100vh"});
   var title = $(".file-details .other-title-bold").text();
 
--- vidz7.com/vidz7.com.user.js
+++ vidz7.com/vidz7.com.user.js
@@ -3,11 +3,71 @@
 // @namespace   vidz7.com
 // @include     http://www.vidz7.com/*
 // @require     https://code.jquery.com/jquery-3.2.1.slim.min.js
-// @version  1
-// @grant    none
+// @version     2
+// @grant       unsafeWindow
 // ==/UserScript==
 
-$(function() {
-	$(".playerplace .dropmenu a").removeAttr("target");
-  $(".hasmenu").replaceWith($(".dropmenu a"));
-})
+$(function () {
+
+  var json = {};
+
+  var textarea = $("<textarea>");
+  textarea.css({ width: "100%" });
+  textarea.on("mouseenter", selectOnHover);
+  $(".widetitle").append(textarea);
+
+  function display(json) {
+    json_string = JSON.stringify(json);
+    textarea.val(json_string);
+  }
+
+  function receiveMessage(event) {
+    console.log("MESSAGE", arguments, event.data);
+    json["duration"] = event.data;
+    display(json);
+  }
+
+  unsafeWindow.addEventListener("message", receiveMessage, false);
+  
+  function selectOnHover(event) {
+    event.target.focus();
+    event.target.select();
+  }
+
+  var url = document.location.href;
+  var poster = $("meta[property='og:image']").attr("content");
+  var id = "vidz7.com-" + $("section.left-column article").attr("id").replace("post-id-", "");
+  var title = $.trim($(".widetitle h1").text());
+  var urls = $.makeArray($(".playerplace .dropmenu a")).map(function(link) {
+    return {url: $(link).attr("href"), text: $.trim($(link).text())};
+  });
+  var duration = 4;
+
+  json["url"] = url;
+  json["poster"] = poster;
+  json["id"] = id;
+  json["title"] = title;
+  json["videoUrls"] = urls;
+
+  display(json);
+
+  // show download links directly
+  $(".playerplace .dropmenu a").removeAttr("target");
+  //$(".hasmenu").replaceWith($(".dropmenu a"));
+  $(".alert").append($(".dropmenu a"));
+
+  // always show full title
+  $(".widetitle").removeClass("widetitle").css({marginTop: "20px"});
+
+  // dont waste so much vertical space
+  $(".header").css({paddingTop: "0"});
+  $("h1").css({fontSize: "18px"});
+  $("section.site").css({padding: "0"});
+  $(".topnav > ul > li a").css({lineHeight: "0"});
+  
+  // full width video
+  $("section.left-column").css({width: "inherit", float: "none"});
+
+  // move alert
+  $(".alert").css({position: "absolute", top: "0", left: "0", width: "150px"});
+});
