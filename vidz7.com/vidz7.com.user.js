e30bd70786a9920de3344257f798280899449cef
Haldir <haldir@haldir.com>
2018-03-24 18:13:47 +0100

    asd

--- vidz7.com/vidz7.com.user.js
+++ vidz7.com/vidz7.com.user.js
@@ -3,6 +3,7 @@
 // @namespace   vidz7.com
 // @include     http://www.vidz7.com/*
 // @require     https://code.jquery.com/jquery-3.2.1.slim.min.js
+// @require     https://raw.githubusercontent.com/lodash/lodash/4.17.5/dist/lodash.min.js
 // @version     3
 // @grant       unsafeWindow
 // ==/UserScript==
@@ -24,15 +25,15 @@ $(function () {
   }
 
   function receiveMessage(event) {
-    console.log("MESSAGE", arguments, event.data);
     if(event.data) {
       headline.css({color: "green"});
-    } else {
+      json["duration"] = event.data;
+      display(json);
+      textarea.select();
+      document.title = "X" + document.title;
+      } else {
       headline.css({color: "red"});
     }
-    json["duration"] = event.data;
-    display(json);
-    textarea.select();
   }
 
   unsafeWindow.addEventListener("message", receiveMessage, false);
@@ -42,6 +43,32 @@ $(function () {
     event.target.select();
   }
 
+  function averageTitle() {
+    // the site scambles the movie title (maybe to throw of copyright bots)
+    // by changing 2 letters in the title. There are several places where the
+    // title is in the HTML, all with different letters scrambled. Collect them
+    // and build the title from the letters occuring most often in a given place
+    var titles = [
+      $("title").text().split("|")[1],
+      $("[property='og:title']").attr("content").split("|")[1],
+      $(".widetitle h1").text()
+    ].map(function(t) {
+      return {original: t, chars: $.trim(t).split("")}
+    });
+    var chars = titles.map(function(t) { return t.chars; })
+    var x = _.zip.apply(_, chars).map(function(pairs) {
+      var countedPairs = _.countBy(pairs);
+      return _.maxBy(Object.keys(countedPairs), function(letter) {
+        return countedPairs[letter];
+      });
+      
+    })
+
+    return x.join("");
+  }
+
+  
+
   function titleToSize(title) {
     return parseInt(title, 10);
   }
@@ -49,7 +76,7 @@ $(function () {
   var url = document.location.href;
   var poster = $("meta[property='og:image']").attr("content");
   var id = "vidz7.com-" + $("section.left-column article").attr("id").replace("post-id-", "");
-  var title = $.trim($(".widetitle h1").text());
+  var title = averageTitle();
   var urls = $.makeArray($(".playerplace .dropmenu a")).map(function(link) {
     return {url: $(link).attr("href"), size: titleToSize($(link).text())};
   });
@@ -82,4 +109,13 @@ $(function () {
 
   // move alert
   $(".alert").css({position: "absolute", top: "0", left: "0", width: "150px"});
+
+  var relatedUrls = $.makeArray($("#relates-posts .thumb")).map(function(a) {
+    return $(a).attr("href");
+  });
+
+  var relatedTextarea = $("<textarea>").val(relatedUrls.join("\n"))
+  relatedTextarea.on("mouseenter", selectOnHover);
+  $(".alert").append(relatedTextarea);
+
 });
---
+++ yourporn.sexy/yourporn.sexy.user.js
@@ -0,0 +1,84 @@
+// ==UserScript==
+// @name         yourporn.sexy
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://yourporn.sexy/post/*
+// @match         https://yourporn.sexy/*
+// @grant        unsafeWindow
+// ==/UserScript==
+
+
+$(function() {
+  function inDiv(text) {
+      return $("<div>").append(text);
+  }
+
+  function selectOnHover(event) {
+      event.target.focus();
+      event.target.select();
+  }
+
+  var VALID_SIZES = [360, 480, 720, 1080];
+
+  function heightToSize(height) {
+    var diffs = VALID_SIZES.map(function(s) { return Math.abs(s - height) });
+    var minDiff = Math.min.apply(Math, diffs);
+    return VALID_SIZES[diffs.indexOf(minDiff)];
+  }
+
+  function onVideoLoaded(video) {
+      var resolution = "" + video.videoWidth + "x" + video.videoHeight;
+      var info = {
+          id: "yourporn.sexy-" + document.location.href.match(/post\/([^.]+)/)[1],
+          videoUrls: [{
+            url: video.src,
+            size: heightToSize(video.videoHeight)
+          }],
+          url: document.location.href,
+          title: $("#posts_container .post_text").text().replace(/[^-\da-z,.']+/ig, " "),
+          poster: video.poster,
+          duration: Math.round(video.duration)
+      };
+      div.empty();
+      div.append(inDiv(resolution));
+      var updateUrl = "http://localhost:3000/videos/update_from_json?json=" + encodeURIComponent(JSON.stringify(info))
+      div.append(inDiv($("<a>").attr("href", updateUrl).text("http://localhost:3000/videos/update_from_json")));
+      var textarea = $("<textarea>").val(JSON.stringify(info));
+      textarea.css({width: "100%"});
+      textarea.on("mouseenter", selectOnHover);
+      div.append(textarea);
+      unsafeWindow.location.href = updateUrl;
+      //div.append(inDiv(JSON.stringify(info)).css({fontSize: "10px"}).on("mouseenter", selectOnHover));
+  }
+
+  var $video = $("video");
+  var video = $video.get(0);
+  if(video) {
+      var div = $("<div>").css({color: "tan", fontSize: "14px"}).text("--x--");
+      $("#h5vwrapper_player_el").prepend(div);
+      var fn = onVideoLoaded.bind(null, video);
+      if(video.readyState != 4) {
+          $video.on("loadeddata", fn);
+      } else {
+          onVideoLoaded(video);
+      }
+  }
+});
+
+function onImageLoad() {
+  var image = $(this);
+  var title = image.parents(".post_block").find(".span_author_name span");
+  title.text("" + this.naturalWidth + "x" + this.naturalHeight);
+}
+
+$(function() {
+  $("img.mini_post_vid_thumb").map(function() {
+      if(this.complete) {
+          onImageLoad.apply(this);
+      } else {
+          $(this).on("load", onImageLoad);
+      }
+  });
+});
