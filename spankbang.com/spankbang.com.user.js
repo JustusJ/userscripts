9fbd0251aeef9417e8b7364a92e8fa8c100d6888
JustusJ <JustusJ@mailinator.com>
2018-04-11 11:06:48 +0200

    asd

--- spankbang.com/spankbang.com.user.js
+++ spankbang.com/spankbang.com.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         spankbang.com
 // @namespace    http://tampermonkey.net/
-// @version      3
+// @version      4
 // @description  try to take over the world!
 // @author       You
 // @match        https://*.spankbang.com/*/video/*
@@ -50,7 +50,7 @@
   ]);
   var duration = $.trim($(".details .right_side span").text());
   var parts = duration.split(":");
-  duration = parseInt(parts[0], 10) * 60 + parseInt(parts[1], 10) * 60;
+  duration = parseInt(parts[0], 10) * 60 + parseInt(parts[1], 10);
 
   var json = JSON.stringify({
       url: url, poster: poster, id: id, title: title, videoUrls: urls, duration: duration
